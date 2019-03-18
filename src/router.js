07e3592aaacfe086a930cad638e57e0f0bac3a3a
naufalafif58 <naufalafif58@gmail.com>
2019-03-18 10:32:18 +0700

    finish crud

--- src/router.js
+++ src/router.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router();
 const {query} = require('./model')
 
-router.route('/books/crud')
-.get((req,res,next)=>{
+
+router.get('/books/crud',(req,res,next)=>{
   let hasil = null
   query((db,resolve,reject)=>{
     db.all('SELECT * from book',[],(err,rows)=>{
@@ -18,7 +18,7 @@ router.route('/books/crud')
     })
   })
 })
-.post((req,res,next)=>{
+router.post('/books/crud',(req,res,next)=>{
   console.log(req.body)
   const {title,desc,release_date,publisher} = req.body
   const insert_query = `INSERT INTO book (book_title,book_desc,release_date,publisher) VALUES ("${title}","${desc}","${release_date}","${publisher}")`
@@ -38,12 +38,59 @@ router.route('/books/crud')
     })
   })
 })
-.put((req,res,next)=>{  
-  res.send('put book')
+
+router.put('/books/crud',(req,res)=>{
+  const {book_id,book_title,book_desc,release_date,publisher} = req.body
+
+  
+  let update_query = `  
+  UPDATE book
+  SET book_title = "${book_title}", 
+      book_desc = "${book_desc}", 
+      release_date = "${release_date}", 
+      publisher = "${publisher}"
+  WHERE book_id = ${book_id} ;
+  `
+
+  console.log(update_query);
+  
+  query((db,resolve,reject)=>{
+    db.run(update_query,[],function(err){
+      if(err){
+        reject(`fail, message : ${err.message}`)
+        res.send({action:false,message:err.message})
+      }else{
+        resolve('success')
+        let message = `A row has been updated with rowid ${book_id}`
+        res.send({action:true,message})
+        console.log(message)
+      }
+    })
+  })
+  
 })
-.delete((req,res,next)=>{
-  res.send('delete book')
+
+router.delete('/books/crud/:id',(req,res)=>{
+  let book_id = req.params.id
+  let delete_query = `DELETE FROM book
+  WHERE book_id = ${book_id};
+  `
+  query((db,resolve,reject)=>{
+    db.run(delete_query,[],function(err){
+      if(err){
+        reject(`fail, message : ${err.message}`)
+        res.send({action:false,message:err.message})
+      }else{
+        resolve('success')
+        let message = `A row has been deleted with rowid ${book_id}`
+        res.send({action:true,message})
+        console.log(message)
+      }
+    })
+  })
+  
 })
+
 
 router.get('/books',(req,res)=>{
   res.render('books',{title:'Books Page'})
